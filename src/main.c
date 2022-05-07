2b42ffa9f05bcd250ec9b8f8ae7bec0a4c03addc
Britannio <britanniojarrett@gmail.com>
2022-05-07 21:05:24 +0100

    Implement human painting

--- src/main.c
+++ src/main.c
@@ -4,8 +4,24 @@
 #include "lib/fonts.h"
 #include "lib/st7735.h"
 
+// Game constants
+const int gridSize = 3;
+
+// Display dimensions
 const u_int16_t width = 80;
 const u_int16_t height = 160;
+
+enum Player
+{
+  empty,
+  ai,
+  human
+};
+
+typedef struct
+{
+  enum Player player;
+} GridPos;
 
 int main()
 {
@@ -17,12 +33,83 @@ int main()
 
   clearScreen();
 
-  paintGrid(3);
+  GridPos grid[] = {
+      (GridPos){.player = human},
+      (GridPos){.player = empty},
+      (GridPos){.player = empty},
+      (GridPos){.player = empty},
+      (GridPos){.player = human},
+      (GridPos){.player = empty},
+      (GridPos){.player = empty},
+      (GridPos){.player = empty},
+      (GridPos){.player = human},
+
+  };
+  paintGrid(gridSize, grid);
 }
 
 void clearScreen()
 {
   ST7735_FillScreen(ST7735_BLACK);
+}
+
+void paintGrid(int size, GridPos grid[])
+{
+  uint16_t oneThird = 27;
+  uint16_t twoThirds = 53;
+
+  // Paint the lines forming the grid
+  paintVerticalLine(oneThird, 0, width);
+  paintVerticalLine(twoThirds, 0, width);
+  paintHorizontalLine(oneThird, 0, width);
+  paintHorizontalLine(twoThirds, 0, width);
+
+  // Paint the players
+  for (int i = 0; i < size * size; i++)
+  {
+    printf("Painting player %d at position %d\n", grid[i].player, i);
+    switch (grid[i].player)
+    {
+    case ai:
+      paintAI(i);
+      break;
+    case human:
+      paintHuman(i);
+      break;
+    case empty:
+      break;
+    }
+  }
+}
+
+void paintHuman(uint8_t pos)
+{
+  const uint16_t oneThird = 26;
+
+  // Paint a square
+  uint16_t x = (pos % 3);
+  x += x * oneThird;
+  uint16_t y = pos / 3;
+  y += y * oneThird;
+
+  // Add inset
+  const uint16_t inset = 5;
+  x += inset;
+  y += inset;
+  paintSquare(x, y, oneThird - (2 * inset));
+}
+
+void paintSquare(uint16_t x, uint16_t y, uint16_t size)
+{
+  paintVerticalLine(x, y, y + size);
+  paintVerticalLine(x + size, y, y + size);
+  paintHorizontalLine(y, x, x + size);
+  paintHorizontalLine(y + size, x, x + size);
+}
+
+void paintAI(uint8_t pos)
+{
+  //
 }
 
 void paintVerticalLine(uint16_t x, uint16_t y1, uint16_t y2)
@@ -53,12 +140,4 @@ void paintHorizontalLine(uint16_t y, uint16_t x1, uint16_t x2)
   {
     ST7735_DrawPixel(x, y, ST7735_WHITE);
   }
-}
-
-void paintGrid(int size)
-{
-  paintVerticalLine(27, 0, width);
-  paintVerticalLine(53, 0, width);
-  paintHorizontalLine(27, 0, width);
-  paintHorizontalLine(53, 0, width);
 }
